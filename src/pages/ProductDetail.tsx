d30efdc22cd698a4572891892220c1b7a247f3ae
Jenish Limbu <jlimbu539@gmail.com>
2024-07-24 11:01:13 +0545

    product detail design completed

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import FooterLinks from "../components/FooterLinks";
-
+import diaper from "../../src/Images/diaper.jpg";
+import { Link } from "react-router-dom";
+import Location from "../Icons/pin.png";
+import Cash from "../Icons/cash.png";
+import Delivery from "../Icons/delivery-truck.png";
+import Info from "../Icons/info.png";
+import Trustedbrand from "../Icons/100-percent.png";
+import Warranty from "../Icons/security.png";
+import Chat from "../Icons/chat.png";
+import Ratingstar from "../Icons/rating-star.png";
 const ProductDetail = () => {
   return (
     <>
@@ -15,42 +24,87 @@ const ProductDetail = () => {
             <div className="col-9">
               <div className="d-flex gap-2">
                 <div className="">
-                  <img src="" alt="Product Image" />
-                  <div className="d-flex gap-2">
-                    <img src="" alt="Img 1" />
-                    <img src="" alt="Img 2" />
-                    <img src="" alt="Img 3" />
+                  <div className="productDetail-productImg">
+                    <img src={diaper} alt="Product Image" />
+                  </div>
+                  <div className="d-flex gap-2 detailed-img-lists">
+                    <img src={diaper} alt="Img 1" />
                   </div>
                 </div>
                 <div>
-                  <h2>
-                    Excellent Quality Random Colour Bath Gloves For Unisexual
+                  <h2 className="productDetail-product-title mb-3">
+                    MAMYPOKO – Pants Style Diapers XL 5pcs
                   </h2>
-                  <div className="product-detail-rating d-flex align-items-center gap-2">
-                    <img src="" alt="Rating Img" />
-                    <span className="productDetail-rating-no">1 Ratings</span>
+                  <div className="product-detail-rating d-flex align-items-center gap-2 mb-1">
+                    <div className="d-flex align-items-center">
+                      <img
+                        src={Ratingstar}
+                        alt="Rating Img"
+                        className="product-detail-title-rating-star"
+                      />{" "}
+                      <img
+                        src={Ratingstar}
+                        alt="Rating Img"
+                        className="product-detail-title-rating-star"
+                      />{" "}
+                      <img
+                        src={Ratingstar}
+                        alt="Rating Img"
+                        className="product-detail-title-rating-star"
+                      />{" "}
+                      <img
+                        src={Ratingstar}
+                        alt="Rating Img"
+                        className="product-detail-title-rating-star"
+                      />{" "}
+                      <img
+                        src={Ratingstar}
+                        alt="Rating Img"
+                        className="product-detail-title-rating-star"
+                      />{" "}
+                    </div>
+                    <span className="productDetail-rating-no">No Ratings</span>
                   </div>
                   <div className="productDetail-brand">
-                    <span>Brand: No Brand</span>
+                    <p style={{ marginBottom: "10px" }}>
+                      <span className="title">Brand:</span>{" "}
+                      <span className="brand-name">No Brand</span>
+                    </p>
                   </div>
-                  <div className="border"></div>
+
                   <div className="productDetail-price">
                     <span className="price">RS. 1,150</span>
                   </div>
-                  <div className="productDetail-promotions">
+                  {/* <div className="productDetail-promotions">
                     <span>Promotions</span>
                     <img src="" alt="Promotion Img" />
-                  </div>
+                  </div> */}
                   <div className="productDetail-purchasing-qty">
-                    <span>Quantity</span>
-                    <div className="d-flex align-items-center gap-1">
-                      <button>-</button>
-                      <span className="productDetail-qty">1</span>
-                      <button>+</button>
+                    <div className="d-flex gap-3 align-items-center">
+                      <span className="w-auto quantity">Quantity</span>
+                      <div className="d-flex align-items-center gap-1">
+                        <button className="productDetail-qty-btns">-</button>
+                        <span className="productDetail-qty">1</span>
+                        <button className="productDetail-qty-btns">+</button>
+                      </div>
                     </div>
-                    <div className="purchasing-btns">
-                      <div className="buy-now">Buy Now</div>
-                      <div className="add-to-cart">Add To Cart</div>
+                    <div className="purchasing-btns d-flex gap-2">
+                      <div className="buy-now text-center d-flex align-items-center justify-content-center">
+                        <Link
+                          to="#"
+                          className="text-light text-decoration-none"
+                        >
+                          Buy Now
+                        </Link>
+                      </div>
+                      <div className="add-to-cart text-center d-flex align-items-center justify-content-center">
+                        <Link
+                          to="#"
+                          className="text-light text-decoration-none"
+                        >
+                          Add To Cart
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -58,103 +112,98 @@ const ProductDetail = () => {
             </div>
             <div className="col-3">
               <div className="delivery">
-                <div>
-                  <span>Delivery</span>
-                  <img src="" alt="Time" />
-                </div>
                 <div className="d-flex align-items-center justify-content-between">
-                  <p className="d-flex align-items-center gap-2">
-                    <img src="" alt="" />
-                    <span>
+                  <span className="delivery-title">Delivery</span>
+                  <img src={Info} alt="Time" className="info-logo" />
+                </div>
+                <div className="d-flex align-items-center justify-content-between delivery-location">
+                  <div className="d-flex align-items-center gap-2">
+                    <img
+                      src={Location}
+                      alt="Location"
+                      className="location-logo"
+                    />
+                    <span className="deliver-subtitles">
                       Bagmati, Kathmandu Metro 22 - Newroad Area, Newroad
                     </span>
-                  </p>
+                  </div>
                   <button>CHANGE</button>
                 </div>
-                <div className="d-flex align-items-center justify-content-between">
-                  <p className="d-flex align-items-center gap-2">
-                    <img src="" alt="" />
-                    <span></span>
-                  </p>
-                  <span>Rs.70</span>
-                </div>
-                <div className="d-flex align-items-center justify-content-between">
-                  <p className="d-flex align-items-center gap-2">
-                    <img src="" alt="" />
-                    <span></span>
-                  </p>
-                  <span>Rs.70</span>
-                </div>
-                <div className="d-flex align-items-center gap-2">
-                  <img src="" alt="" />
-                  <span>Cash On Delivery Available</span>
+
+                <div className="d-flex align-items-center gap-2 product-detail-payment-methods">
+                  <img src={Cash} alt="COD" className="cash-on-delivery" />
+                  <span className="deliver-subtitles">
+                    Cash On Delivery Available
+                  </span>
                 </div>
               </div>
               <div className="service">
                 <div className="service d-flex -align-items-center justify-content-between">
-                  <span>Service</span>
-                  <img src="" alt="Service Img" />
+                  <span className="service-title">Service</span>
+                  <img src={Info} alt="Info Img" className="info-logo" />
                 </div>
+                <div className="d-flex gap-2 pb-2">
+                  <img
+                    src={Trustedbrand}
+                    alt="Trusted Brand"
+                    className="trusted-brand-logo"
+                  />
+                  <span className="service-subtitles">
+                    100% Authentic From Trusted Brand
+                  </span>
+                </div>
+
                 <div className="d-flex gap-2">
-                  <img src="" alt="" />
-                  <div>
-                    <p>100% Authentic From Trusted Brand</p>
-                    <span>or Get 2x Your Money Back</span>
-                  </div>
-                </div>
-                <div className="d-flex gap-2">
-                  <img src="" alt="" />
-                  <div>
-                    <p>14 days free & easy return</p>
-                    <span>Change of mind is not applicable</span>
-                  </div>
-                </div>
-                <div className="d-flex gao-2">
-                  <img src="" alt="Warrant Img" />
-                  <span>Warranty Not available</span>
+                  <img
+                    src={Warranty}
+                    alt="Warrant Img"
+                    className="warranty-logo"
+                  />
+                  <span className="service-subtitles">
+                    Warranty Not available
+                  </span>
                 </div>
               </div>
               <div className="sold-by">
-                <div className="d-flex justify-content-between">
+                <div>
                   <div>
-                    <span>Sold by</span>
-                    <p>CONTROL</p>
-                    <img src="" alt="Mall" />
+                    <span className="sold-by-title">Sold by</span>
                   </div>
-                  <div>
-                    <img src="" alt="Chat Logo" />
-                    <span>CHAT</span>
-                  </div>
-                </div>
-                <div className="d-flex">
-                  <div>
-                    <span>Positive Seller Ratings</span>
-                    <h5>91%</h5>
-                  </div>
-                  <div>
-                    <span>Ship on Time</span>
-                    <h5>91%</h5>
-                  </div>
-                  <div>
-                    <span>Chat Response Rate</span>
-                    <h5>64%</h5>
+                  <div className="d-flex align-items-center justify-content-between pt-1">
+                    <p className="product-detail-seller-name">CONTROL</p>
+
+                    <div className="d-flex gap-1">
+                      <img src={Chat} alt="Chat Logo" className="chat-logo" />
+                      <span className="chat-title">CHAT</span>
+                    </div>
                   </div>
                 </div>
+                <table className="product-detail-table">
+                  <tbody>
+                    <tr>
+                      <td className="border-left-0">
+                        <span>Positive Seller Ratings</span>
+                        <p className="pt-3">88%</p>
+                      </td>
+                      <td>
+                        <span>Ship On Time</span>
+                        <p className="not-enough-data pt-3">not enough data</p>
+                      </td>
+                      <td className="border-right-0">
+                        <span>Chat Response Rate</span>
+                        <p className="pt-3">10%</p>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
             <div className="col-12 bg-seconday"></div>
             <div className="col-10">
-              <p>Ratings & Reviews</p>
+              <p className="rating-reviews-title">Ratings & Reviews</p>
               <div className="ratings-counter d-flex">
-                <div>
-                  <p className="d-flex gap-2">
-                    <h4>4.9</h4>
-                    <img src="" alt="" />
-                  </p>
-                  <img src="" alt="" />
-                  <span>9 Ratings</span>
-                </div>
-                <div>
+                <span>This product has no reviews.</span>
+                {/* <div>
                   <div className="rating-imgs">
                     <img src="" alt="" />
                     <img src="" alt="" />
@@ -169,19 +218,18 @@ const ProductDetail = () => {
                     <img src="" alt="" />
                     <img src="" alt="" />
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
             <div className="col-2">
-              <h5>People Who Viewed This....</h5>
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
+              <h5 className="people-who-viewed-this">
+                People Who Viewed This....
+              </h5>
+              <div className="card who-viewed-this-card">
+                <img src={diaper} className="card-img-top" alt="..." />
                 <div className="card-body">
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <p>Rs. 409</p>
+                  <p className="card-text">Mamy Poko Pants</p>
+                  <p className="who-viewed-this-price">Rs. 409</p>
                   <img src="" alt="" />
                 </div>
               </div>
